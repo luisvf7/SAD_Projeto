90fa968c426b92b1968f166b3a4c0ca7f63f9ec6
rikyroda <rikymsr@gmail.com>
2017-10-21 18:50:12 +0100

    Exs acrescentados
    Acrescentei os ex 1.4, 2.1 e 2.2

--- proj63/ProjSAD63.sql
+++ proj63/ProjSAD63.sql
@@ -1,32 +1,28 @@
-
-__Nº Turnos p/ Tipo na UC SAD__
-```sql
+--  1.1
 SELECT u.nomeuc, t.tipoturno, count(*)
 FROM ei_sad_proj_gisem.v_turnos t
 	JOIN ei_sad_proj_gisem.v_ucs u ON (t.uc_id = u.id)
 WHERE u.id = 229
 GROUP BY u.nomeuc, t.tipoturno;
-```
-__Nº Estudantes em cada Turno__
-```sql
+
+--  1.2
 SELECT t.id, t.turnouc ,count(*)
 FROM ei_sad_proj_gisem.v_turnos t
 	JOIN ei_sad_proj_gisem.v_turno_user tu ON (t.id = tu.turno_id)
 WHERE t.uc_id = 229
 GROUP BY t.id, t.turnouc;
-```
 
+--  1.3
+SELECT t.anouc, t.tipoturno, count(*)
+FROM ei_sad_proj_gisem.v_turnos t
+	JOIN ei_sad_proj_gisem.v_turno_user tu ON (t.id = tu.turno_id)
+GROUP BY t.anouc, t.tipoturno;
 
-__Nº Minimo e Máximo de Estudantes Inscritos nos Turnos p/ TipoTurno , Ano e Curso__
-
-```sql
 SELECT t.anouc AS anoUc, t.tipoturno AS tipoTurno , t.turnouc AS turnoUc, count(*) AS numEstudantes
 FROM ei_sad_proj_gisem.v_turnos t
 	JOIN ei_sad_proj_gisem.v_turno_user tu ON (t.id = tu.turno_id)
-GROUP BY t.anouc, t.turnouc, t.tipoturno; 
-```
-**Resultado Final**
-```sql
+GROUP BY t.anouc, t.turnouc, t.tipoturno;   
+
 SELECT anoUc, tipoTurno, MIN(numEstudantes), MAX(numEstudantes)
 FROM (SELECT t.anouc AS anoUc, t.tipoturno AS tipoTurno , t.turnouc AS turnoUc, count(*) AS numEstudantes
         FROM ei_sad_proj_gisem.v_turnos t
@@ -34,11 +30,18 @@ FROM (SELECT t.anouc AS anoUc, t.tipoturno AS tipoTurno , t.turnouc AS turnoUc, 
         GROUP BY t.anouc, t.turnouc, t.tipoturno)
 GROUP BY anoUc, tipoTurno
 ORDER BY 1;
-```
 
-__Nº Minimo e Máximo de Estudantes presentes nas Aulas p/ Turno , em SAD__
+--  2.1
+SELECT t.turnouc, aulas.num_presencas, aulas.semana, aulas.diasemana
+FROM ei_sad_proj_gisem.v_turnos t JOIN ei_sad_proj_gisem.v_aulas_semana aulas ON (t.id = aulas.turno_id)
+WHERE t.uc_id = 229
+ORDER BY 3;
 
-```sql
+SELECT t.turnouc AS turnoUc, aulas.num_presencas AS numPresencas
+FROM ei_sad_proj_gisem.v_turnos t JOIN ei_sad_proj_gisem.v_aulas_semana aulas ON (t.id = aulas.turno_id)
+WHERE t.uc_id = 229
+;
+
 SELECT turnoUc, MIN(numPresencas), MAX(numPresencas)
 FROM (SELECT t.turnouc AS turnoUc, aulas.num_presencas AS numPresencas
         FROM ei_sad_proj_gisem.v_turnos t JOIN ei_sad_proj_gisem.v_aulas_semana aulas ON (t.id = aulas.turno_id)
@@ -46,4 +49,3 @@ FROM (SELECT t.turnouc AS turnoUc, aulas.num_presencas AS numPresencas
 GROUP BY turnoUc
 ORDER BY 1
 ;
-```
